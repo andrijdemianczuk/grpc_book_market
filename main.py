df16ddc58138afe1005293e40e5b1e51817a01da
andrijdemianczuk <andrij.demianczuk@gmail.com>
2024-06-23 19:41:42 -0600

    added bootstrap for server code.

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import grpc
+from recommendations.recommendations_pb2_grpc import RecommendationsStub
+from recommendations.recommendations_pb2 import BookCategory, RecommendationRequest
+
 
 if __name__=='__main__':
-    print('hi')
+    channel = grpc.insecure_channel("localhost:50051")
+    client = RecommendationsStub(channel)
+
+    request = RecommendationRequest(user_id=1, category=BookCategory.SCIENCE_FICTION, max_results=3)
+    client.Recommend(request)
